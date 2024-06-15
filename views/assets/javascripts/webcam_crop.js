cc66c4fd96cd6dd5e7e75862a8054322da6bd44e
Reiza Dwitama <reiza.dwitama12@gmail.com>
2024-06-16 01:28:31 +0700

    add auto_predict

--- views/assets/javascripts/webcam_crop.js
+++ views/assets/javascripts/webcam_crop.js
@@ -1,8 +1,10 @@
 let cropper = "";
-let cropper_predict = "";
+let intervalId;
+let hiddenCropper="";
 let model;
 let cropperData;
 const previewImage = document.getElementById("preview-image");
+const hiddenImage = document.getElementById('preview-image-crop');
 const test = document.getElementById("test");
 const webcam = new Webcam(document.getElementById('wc'));
 
@@ -31,6 +33,51 @@ async function captureNow(){
 // - column
 // - row
 
+async function startInterval(){
+  let count = 0;
+  intervalId = setInterval(async () => {
+      const dataURL = await webcam.captureWithoutTensorFlow();
+      hiddenImage.src = dataURL;
+      
+      if(hiddenCropper){
+        hiddenCropper.destroy();
+      }
+      hiddenCropper = new Cropper(hiddenImage);
+
+      await fetch('/park_slot')
+        .then(response => response.json()) // Parse the JSON from the response
+        .then(data => {cropperData = data})   // Do something with the data
+        .catch(error => console.error('Error:', error)); // Handle errors
+
+      console.log(cropperData.length);
+
+      for (let i in cropperData) {
+        hiddenCropper.setCropBoxData(cropperData[i]);
+        let img = hiddenCropper.getCroppedCanvas();
+        let classId = await predict(img);
+        console.log(classId);
+        await fetch('/update', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            id: i.toString(),
+            class: classId,
+          }),
+        })
+        .then(response => response.text())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+      }
+
+  }, 5000);
+}
+
+function stopInterval(){
+  clearInterval(intervalId);
+}
+
 async function cropCapture(){
   if(cropper){
     let data = cropper.getCropBoxData();//JSON
@@ -45,21 +92,12 @@ async function cropCapture(){
 
 function convertToTf(imgSrc){
   return tf.tidy(() => {
-    // Reads the image as a Tensor from the webcam <video> element.
     const webcamImage = tf.browser.fromPixels(imgSrc);
-
     const reversedImage = webcamImage.reverse(1);
-
-    // Crop the image so we're using the center square of the rectangular
-    // webcam.
     const croppedImage = webcam.cropImage(reversedImage);
-
-    // Expand the outer most dimension so we have a batch size of 1.
-    const batchedImage = croppedImage.expandDims(0);
-
-    // Normalize the image between -1 and 1. The image comes in between 0-255,
-    // so we divide by 127 and subtract 1.
-    return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    const expandedImg = croppedImage.expandDims(0);
+    const batchedImg =  expandedImg.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    return tf.image.resizeBilinear(batchedImg,[150, 150]);
   });
 }
 
@@ -75,16 +113,12 @@ function cropReset(){
 
 async function predict(img){
   let processedImg = convertToTf(img);
-  let resizedTensorFrame = tf.image.resizeBilinear(processedImg,[150,
-    150]);
-
-  let predictions = await model.predict(resizedTensorFrame);
-  // const myJSON = JSON.stringify(predictions.argMax());
+  let predictions = await model.predict(processedImg);
   const classId = (await predictions.data())[0];
   if(classId >= 10e-20){
-    return 0;
+    return classId;
   }else {
-    return 1;
+    return classId;
   }
 }
 
