814c0138acb4b105eb8915a815a0f846049de7d1
Reiza Dwitama <reiza.dwitama12@gmail.com>
2024-06-16 22:08:16 +0700

    Fixed Crop Image

--- server.js
+++ server.js
@@ -5,41 +5,40 @@ const path = require('path');
 app.use('/css', express.static(path.join(__dirname, 'views/assets/css')));
 app.use('/js', express.static(path.join(__dirname, 'views/assets/javascripts')));
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
-});
 
-app.get('/tes', (req, res) => {
-  const result = 'Hello from myFunction!';
-  res.send(result);
-});
+// app.get('/tes', (req, res) => {
+//   const result = 'Hello from myFunction!';
+//   res.send(result);
+// });
 
 app.get('/fetch', (req, res) => {
   res.sendFile(path.join(__dirname,'views/fetch.html'));
 });
 
-app.get('/submit', (req, res) => {
-    res.sendFile(path.join(__dirname,'views/form.html'));
-});
-
-app.get('/webcam', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'views/webcam_crop.html'));
 });
 
 app.get('/park_slot', (req, res) => {
   const data = {
-    "1": {
-        "left": "204.5",
-        "top": "230",
-        "width": "155",
-        "height": "209"
+    1: {
+      "left": 886.75,
+      "top": 256,
+      "width": 31,
+      "height": 56
+  },
+    2: {
+      "left": 888.75,
+      "top": 365,
+      "width": 31,
+      "height": 56
     },
-    "2": {
-        "left": "255.5",
-        "top": "261",
-        "width": "156",
-        "height": "175"
-    }
+    3: {
+      "left": 890.75,
+      "top": 419,
+      "width": 31,
+      "height": 56
+  }
   };
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(data));
@@ -48,11 +47,8 @@ app.get('/park_slot', (req, res) => {
 // This middleware is available in Express v4.16.0 onwards
 app.use(express.urlencoded({extended: true}));
 
-app.post('/submit', (req, res) => {
-  console.log({
-    name: req.body.name,
-    message: req.body.message,
-  });
+app.post('/add_data', (req, res) => {
+  console.log(req.body);
   res.redirect('/thankyou');
 });
 
@@ -60,8 +56,8 @@ app.post('/update', (req, res) => {
   console.log(req.body);
 });
 
-app.get('/thankyou', (req, res) => {
-    res.redirect('/submit');
+app.get('/parking_slot', (req, res) => {
+  //query
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
--- views/assets/javascripts/webcam_crop.js
+++ views/assets/javascripts/webcam_crop.js
@@ -1,10 +1,9 @@
 let cropper = "";
-let hidden_cropper = "";
+let dataToSend;
+let intervalId;
+const canvas = document.getElementById('canvas');
 let model;
-let worker;
 const previewImage = document.getElementById("preview-image");
-const hiddenImage = document.getElementById("preview-image-crop");
-const test = document.getElementById("test");
 const webcam = new Webcam(document.getElementById('wc'));
 
 async function init(){
@@ -12,38 +11,6 @@ async function init(){
   model = await tf.loadLayersModel('js/tf_model/model.json');
   console.log(model.summary());
 
-}
-
-async function startWorker(){
-  console.log('pressed');
-  
-  if (window.Worker) {
-      worker = new Worker('js/worker.js');
-      setInterval(async () => {
-        const dataURL = await webcam.captureWithoutTensorFlow();
-        const parameters = {dataURL, model};
-        worker.postMessage(parameters);
-      }, 10000); // Run every 10 seconds
-      
-      worker.onmessage = function(event) {
-          console.log("berhasil");
-      };
-
-      worker.onerror = function(error) {
-          console.error('Worker error:', error);
-      };
-  } else {
-      console.error('Web Workers are not supported in your browser.');
-  }
-}
-
-function stopWorker(){
-  
-  if (worker) {
-    console.log('pressed');
-    worker.terminate();
-    worker = null;
-  }
 }
 
 async function captureNow(){
@@ -56,31 +23,150 @@ async function captureNow(){
   cropper = new Cropper(previewImage);
 
 }
-// - id (unique)
-// - left
-// - top
-// - width
-// - height
-// - column
-// - row
+
+async function startInterval() {
+  let cropperData;
+  intervalId = setInterval(async () => {
+    console.log('halo');
+    const ctx = canvas.getContext('2d');
+    const dataURL = await webcam.captureWithoutTensorFlow(); // an URL of an image captured from webcam
+    
+    const img = new Image();
+    img.src = dataURL;
+
+    await new Promise((resolve) => {
+      img.onload = resolve;
+    });
+
+    // GET CROPPED DATA
+    await fetch('/park_slot')
+        .then(response => response.json()) // Parse the JSON from the response
+        .then(data => {cropperData = data})   // Do something with the data
+        .catch(error => console.error('Error:', error)); // Handle errors
+
+    console.log(cropperData);
+
+    for (let i in cropperData) {
+      console.log(i);
+
+      let { left, top, width, height } = cropperData[i];
+      left = left * 1.1862836;
+      top = top * 1.1862836;
+      width = width * 1.1862836;
+      height = height * 1.1862836;
+
+      const offscreenCanvas = document.createElement('canvas');
+      const offscreenCtx = offscreenCanvas.getContext('2d');
+      offscreenCanvas.width = width;
+      offscreenCanvas.height = height;
+      offscreenCtx.drawImage(img, left, top, width, height, 0, 0, width, height);
+      const croppedImageURL = offscreenCanvas.toDataURL();
+      const croppedImg = new Image();
+      croppedImg.src = croppedImageURL;
+      await new Promise((resolve) => {
+        croppedImg.onload = resolve;
+      });
+
+      let classId = await predict(croppedImg);
+      console.log(classId);
+
+      fetch('/update', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          id: i.toString(),
+          class: classId,
+        }),
+      })
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+    }
+
+  }, 5000);
+}
+
+
+function stopInterval(){
+  clearInterval(intervalId);
+}
 
 async function cropCapture(){
   if(cropper){
-    let data = cropper.getCropBoxData();//JSON
+    let data = cropper.getCropBoxData();
     console.log(data);
     const column = document.getElementById("column_input");
     const row = document.getElementById("row_input");
+
+    dataToSend = {
+      "left" : data['left'],
+      "top" : data['top'],
+      "width" : data['width'],
+      "height" : data['height'],
+      "row" : row,
+      "column" : column
+    }
+    
+
   }
 }
 
+function convertToTf(imgSrc){
+  return tf.tidy(() => {
+    const webcamImage = tf.browser.fromPixels(imgSrc);
+    const reversedImage = webcamImage.reverse(1);
+    const croppedImage = webcam.cropImage(reversedImage);
+    const expandedImg = croppedImage.expandDims(0);
+    const batchedImg =  expandedImg.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    return tf.image.resizeBilinear(batchedImg,[150, 150]);
+  });
+}
+
 function sendData(){
-  console.log("Pressed Send Data")
+  if(dataToSend){
+    fetch('/add_slot', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        left : data['left'],
+        top : data['top'],
+        width : data['width'],
+        height : data['height'],
+        row : row,
+        column : column
+      }),
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+    cropReset();
+  }
 }
 
 function cropReset(){
   if(cropper){
     cropper.reset();
+    dataToSend = null;
   }
 }
+
+async function predict(img){
+  let classId;
+  tf.tidy(async () => {
+  let processedImg = convertToTf(img);
+  let predictions = await model.predict(processedImg);
+  classId = (await predictions.data())[0];
+  });
+  if(classId >= 10e-20){
+    return 1;
+  }else {
+    return 0;
+  }
+}
+
 
 init();
